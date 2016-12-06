af4e76416dbf426f43e6277579fb603b8a32e156
Meowth52 <meowth@meowth.se>
2016-12-06 01:15:31 +0100

    Dag 2 del 2
    wee hexadecimal!

--- Advent2016/Day2.cs
+++ Advent2016/Day2.cs
@@ -8,6 +8,7 @@ namespace Advent2016
         private string[] Instructions;
         private string TheCode;
         private Button[] Codepad=new Button[10];
+        private Button[] Codepad2 = new Button[14];
 
         public Day2(string input)
         {
@@ -23,11 +24,26 @@ namespace Advent2016
             Codepad[7] = new Button(7, 4, 8, 7, 7);
             Codepad[8] = new Button(8, 5, 9, 8, 7);
             Codepad[9] = new Button(9, 6, 9, 9, 8);
+
+            Codepad2[1] = new Button(1, 1, 1, 3, 1);
+            Codepad2[2] = new Button(2, 2, 3, 6, 2);
+            Codepad2[3] = new Button(3, 1, 4, 7, 2);
+            Codepad2[4] = new Button(4, 4, 4, 8, 3);
+            Codepad2[5] = new Button(5, 5, 6, 5, 5);
+            Codepad2[6] = new Button(6, 2, 7, 10, 5);
+            Codepad2[7] = new Button(7, 3, 8, 11, 6);
+            Codepad2[8] = new Button(8, 4, 9, 12, 8);
+            Codepad2[9] = new Button(9, 9, 9, 9, 8);
+            Codepad2[10] = new Button(10, 6, 11, 10, 10);
+            Codepad2[11] = new Button(11, 7, 12, 13, 10);
+            Codepad2[12] = new Button(12, 8, 12, 12, 11);
+            Codepad2[13] = new Button(13, 11, 13, 13, 13);
         }
 
         internal string Result()
         {
             string resultat="";
+            string resultat2 = "";
             int n = 5;
             foreach (string s in Instructions)
             {
@@ -37,9 +53,21 @@ namespace Advent2016
                 {
                     n = Codepad[n].move(c);
                 }
-                resultat = string.Concat(resultat, n.ToString());
+                resultat = string.Concat(resultat, n.ToString("X"));
             }
-            return "Dag 2, koden är: " + resultat;
+
+            n = 5;
+            foreach (string s in Instructions)
+            {
+                if (s == "")
+                    break;
+                foreach (char c in s)
+                {
+                    n = Codepad2[n].move(c);
+                }
+                resultat2 = string.Concat(resultat2, n.ToString("X"));
+            }
+            return "Dag 2, koden är: " + resultat + " och för del 2: " + resultat2;
         }
     }
 }
