17d22faaa95dda365c47074601857ebf469c60ac
Meowth52 <meowth@meowth.se>
2017-11-19 01:18:35 +0100

    Dag 11 soon™

--- Advent2016/Day11.cs
+++ Advent2016/Day11.cs
@@ -11,7 +11,7 @@ namespace Advent2016
     {
         private string Input;
         private string[] Instructions;
-        public List<Component>[] Floors = { new  List<Component>(), new List<Component>(), new List<Component>(), new List<Component>()};
+        public List<Component>[] Floors = { new  List<Component>(), new List<Component>(), new List<Component>(), new List<Component>(), new List<Component>() };
         int FloorCounter=0;
         public Day11(string input)
                 {
@@ -20,24 +20,43 @@ namespace Advent2016
             foreach(string s in Instructions)
             {
                 FloorCounter++;
-                string[] Words;
-                Words = s.Split(' ');
-                string Lastword = "";
-                foreach (string word in Words)
+                Regex theMatch = new Regex(@"(\w+)(?= generator)");
+                foreach (Match m in theMatch.Matches(s))
                 {
-                    if (word = "generator")
-
-                    Lastword = word;
+                    Floors[FloorCounter].Add(new Component("generator", m.ToString()));
                 }
-                Regex theMatch = new Regex(@"(\d+)\D+(\d+)");
+                Regex theMatch2 = new Regex(@"(\w+)(?=-compatible)");
+                foreach (Match m in theMatch2.Matches(s))
+                {
+                    Floors[FloorCounter].Add(new Component("microchip", m.ToString()));
+                }
             }
-            Floors[1].Add();
 
         }
 
         internal string Result()
         {
-            throw new NotImplementedException();
+            int StepCounter = 0;
+            FloorCounter = 0;
+            List<Component> FloorCopy = new List<Component>();
+            while (Floors[1].Any() || Floors[2].Any() || Floors[3].Any())
+            {
+                FloorCopy.Clear();
+                FloorCounter++;
+                if (FloorCounter >= 4)
+                    FloorCounter = 1;
+                foreach(Component c in Floors[FloorCounter])
+                {
+                    FloorCopy.Add(c);
+                }
+                foreach(Component c in FloorCopy)
+                {
+                    Floors[FloorCounter].Remove(c);
+                    Floors[FloorCounter + 1].Add(c);
+                }
+
+            }
+            return StepCounter.ToString();
         }
     }
 }
