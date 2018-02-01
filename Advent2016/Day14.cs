5a4cb60739b5828089f0f8f5737e6e54c66d1704
Meowth52 <meowth@meowth.se>
2018-02-01 11:36:43 +0100

    arghh!!

--- Advent2016/Day14.cs
+++ Advent2016/Day14.cs
@@ -21,61 +21,66 @@ namespace Advent2016
             int Sum = 0;
             int Sum2 = 0;
             int Iterator = 0;
+            int EndCounter = 0;
             byte[] byteData;
             string TestString;
-            int KeyCounter = 0;
+            List<int> RemoveKeys = new List<int>();
             MD5 hashis = MD5.Create();
             StringBuilder HexString = new StringBuilder();
-            string TestBuilder;
-            List<string> TrippleTest = new List<string>();
-            //List<string> FrippleTest = new List<string>();
             Dictionary<int, string> FrippleTestContenders = new Dictionary<int, string>();
-            for(int i = 0; i < 16; i++)
-            {
-                TestBuilder = Convert.ToString(i, 16);
-                TestBuilder += TestBuilder + TestBuilder;
-                TrippleTest.Add(TestBuilder);
-            }
-            //for (int i = 0; i < 16; i++)
-            //{
-            //    TestBuilder = Convert.ToString(i, 16);
-            //    TestBuilder += TestBuilder + TestBuilder + TestBuilder + TestBuilder;
-            //    FrippleTest.Add(TestBuilder);
-            //}
-
+            Dictionary<int, string> KeyContenders = new Dictionary<int, string>();
             while (true)
             {
                 HexString.Clear();
                 byteData = hashis.ComputeHash(Encoding.UTF8.GetBytes(Salt + Iterator.ToString()));
                 int ByteLenght = byteData.Length;
-                for (int i = 0; i < ByteLenght-2; i += 2)
-                {                   
-                    ushort Hashnumber = BitConverter.ToUInt16(byteData, i);
-                    HexString.Append(Convert.ToString(Hashnumber, 16));
+                for (int i = 0; i < byteData.Length; i++)
+                {
+                    HexString.Append(byteData[i].ToString("x2"));
                 }
                 TestString = HexString.ToString();
-                foreach(string s in TrippleTest)
+                if (EndCounter == 0)
                 {
-                    if (TestString.Contains(s))
+                    char LastChar = 'x';
+                    char EvenLasterChar = 'y';
+                    foreach (char c in TestString)
                     {
-                        FrippleTestContenders.Add(Iterator, s + s[0] + s[0]);
-                        break;
+                        if (c == LastChar && c== EvenLasterChar)
+                        {
+                            FrippleTestContenders.Add(Iterator, c.ToString() + c + c + c + c);
+                            break;
+                        }
+                        EvenLasterChar = LastChar;
+                        LastChar = c;
                     }
                 }
                 FrippleTestContenders.Remove(Iterator-1000);
-                foreach(KeyValuePair<int, string> s in FrippleTestContenders)
+                foreach (KeyValuePair<int, string> s in FrippleTestContenders)
                 {
                     if (TestString.Contains(s.Value))
                     {
-                        KeyCounter++;
-                        Sum = s.Key;
-                        break;
+                        KeyContenders.Add(s.Key, s.Value);
+                        RemoveKeys.Add(s.Key);
                     }
                 }
-                if (KeyCounter >= 1)
+                foreach(int i in RemoveKeys)
+                {
+                    FrippleTestContenders.Remove(i);
+                }
+                RemoveKeys.Clear();
+                if (KeyContenders.Count >= 64)
+                    EndCounter++;
+                if (EndCounter >= 1000)
                     break;
                 Iterator++;
             }
+            List<int> ResultIndex = new List<int>();
+            foreach(KeyValuePair<int,string> k in KeyContenders)
+            {
+                ResultIndex.Add(k.Key);
+            }
+            ResultIndex.Sort();
+            Sum = ResultIndex[63];
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             return "Del 1: " + Sum + " och del 2: " + Sum2 + " Executed in " + ts.TotalMilliseconds.ToString() + " ms";
