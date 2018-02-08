2734335f649e8ea44afd416f8ed0563247c2392d
Meowth52 <meowth@meowth.se>
2018-02-08 16:38:52 +0100

    bf

--- Advent2016/Day21.cs
+++ Advent2016/Day21.cs
@@ -28,7 +28,7 @@ namespace Advent2016
         public string Result()
         {
             string Sum = "";
-            int Sum2 = 0;
+            string Sum2 = "";
             string Keyword = "";
             string PasswordInput = "abcdefgh";
             List<char> Password = new List<char>();
@@ -52,8 +52,10 @@ namespace Advent2016
                         Password[Y] = BufferChar;
                         break;
                     case "swapletter":
-                        Password[Password.IndexOf(s[2][0])] = s[5][0];
-                        Password[Password.IndexOf(s[5][0])] = s[2][0];
+                        X = Password.IndexOf(s[2][0]);
+                        Y = Password.IndexOf(s[5][0]);
+                        Password[X] = s[5][0];
+                        Password[Y] = s[2][0];
                         break;
                     case "rotateleft":
                         Int32.TryParse(s[2], out X);
@@ -105,6 +107,83 @@ namespace Advent2016
             foreach(char c in Password)
             {
                 Sum += c;
+            }
+            //del 2
+            Instructions.Reverse();
+            PasswordInput = "fbgdceah";
+            Password.Clear();
+            foreach (char c in PasswordInput)
+            {
+                Password.Add(c);
+            }
+            foreach (string[] s in Instructions)
+            {
+                Keyword = s[0] + s[1];
+                switch (Keyword)
+                {
+                    case "swapposition":
+                        Int32.TryParse(s[2], out X);
+                        Int32.TryParse(s[5], out Y);
+                        BufferChar = Password[X];
+                        Password[X] = Password[Y];
+                        Password[Y] = BufferChar;
+                        break;
+                    case "swapletter":
+                        X = Password.IndexOf(s[2][0]);
+                        Y = Password.IndexOf(s[5][0]);
+                        Password[X] = s[5][0];
+                        Password[Y] = s[2][0];
+                        break;
+                    case "rotateleft":
+                        Int32.TryParse(s[2], out X);
+                        for (int i = 0; i < X; i++)
+                        {
+                            Password.Add(Password[0]);
+                            Password.RemoveAt(0);
+                        }
+                        break;
+                    case "rotateright":
+                        Int32.TryParse(s[2], out X);
+                        for (int i = 0; i < X; i++)
+                        {
+                            Password.Insert(0, Password[Password.Count - 1]);
+                            Password.RemoveAt(Password.Count - 1);
+                        }
+                        break;
+                    case "rotatebased":
+                        X = Password.IndexOf(s[6][0]);
+                        if (X >= 4)
+                            X++;
+                        X++;
+                        for (int i = 0; i < X; i++)
+                        {
+                            Password.Insert(0, Password[Password.Count - 1]);
+                            Password.RemoveAt(Password.Count - 1);
+                        }
+                        break;
+                    case "reversepositions":
+                        Int32.TryParse(s[2], out X);
+                        Int32.TryParse(s[4], out Y);
+                        List<char> Reversed = new List<char>(Password.GetRange(X, Y - X + 1));
+                        Password.RemoveRange(X, Y - X + 1);
+                        Reversed.Reverse();
+                        Password.InsertRange(X, Reversed);
+                        break;
+                    case "moveposition":
+                        Int32.TryParse(s[2], out X);
+                        Int32.TryParse(s[5], out Y);
+                        BufferChar = Password[X];
+                        Password.RemoveAt(X);
+                        Password.Insert(Y, BufferChar);
+                        break;
+                    default:
+                        ;
+                        break;
+                }
+            }
+            foreach (char c in Password)
+            {
+                Sum2 += c;
             }
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
